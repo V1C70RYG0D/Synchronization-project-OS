c5a923fa594c5b12989f49436cb05b8e089a2b45
VictoryGod <96035378+V1C70RYG0D@users.noreply.github.com>
2023-03-13 18:41:13 +0000

    Sushi

--- SushiBar.c
+++ SushiBar.c
@@ -1,98 +1,96 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdatomic.h>
 #include <unistd.h>
+#define NO_OF_CUSTOMERS 8
 
-//Semaphore struct
-typedef volatile struct
-{
-    volatile atomic_int val;
-    volatile atomic_flag lock;
-} semaphore;
+pthread_t customers[NO_OF_CUSTOMERS];
+int eating = 0, waiting = 0;
 
-semaphore CSmutex; // Semaphore to control access to Critical Section
-semaphore Rmutex; // Semaphore to increment or decrement Reader Count by readers
-semaphore EntryMutex; // Helps make the Reader Writer problem STARVE FREE by assigning equal priority to both
-semaphore waiting;
+typedef struct {
+    int value;
+    pthread_mutex_t mutex;
+    pthread_cond_t cond;
+} custom_semaphore;
 
-int eating = 0, waiting_count = 0;
+custom_semaphore block, mutex;
 
-// wait
-int wait(semaphore *s)
-{
-    while (atomic_flag_test_and_set(&s->lock));
-
-    while (atomic_load(&s->val) <= 0);
-
-    atomic_fetch_sub(&s->val, 1);
-    atomic_flag_clear(&s->lock);
-
-    return 0;
+void init(custom_semaphore* sem, int value) {
+    sem->value = value;
+    pthread_mutex_init(&sem->mutex, NULL);
+    pthread_cond_init(&sem->cond, NULL);
 }
 
-// signal
-int signal(semaphore *s)
-{
-    return atomic_fetch_add(&s->val, 1);
+void wait(custom_semaphore* sem) {
+    pthread_mutex_lock(&sem->mutex);
+    while (sem->value == 0) {
+        pthread_cond_wait(&sem->cond, &sem->mutex);
+    }
+    sem->value--;
+    pthread_mutex_unlock(&sem->mutex);
 }
 
-// Sushi bar function
+void signal(custom_semaphore* sem) {
+    pthread_mutex_lock(&sem->mutex);
+    sem->value++;
+    pthread_cond_signal(&sem->cond);
+    pthread_mutex_unlock(&sem->mutex);
+}
+
 void *sushi_bar(void *arg)
 {
-    int id = *(int *)arg;
     while (1)
     {
-        printf("Customer %d trying to enter\n", id);
-        wait(&EntryMutex);
-        printf("Customer %d entered\n", id);
-        wait(&Rmutex);
+        int i, n;
+        wait(&mutex);
         if (eating == 5)
         {
-            signal(&Rmutex);
-            printf("Customer %d is waiting...\n", id);
-            wait(&waiting);
-            wait(&Rmutex);
-        }
-        signal(&EntryMutex);
-        printf("Customer %d starting to eat\n", id);
-        eating += 1;
-        signal(&Rmutex);
-        sleep(3);
-        printf("Customer %d finished eating\n", id);
-        wait(&Rmutex);
-        eating -= 1;
-        if (waiting_count > 0)
-        {
-            signal(&waiting);
-            waiting_count -= 1;
+            waiting += 1;
+            signal(&mutex);
+            printf("waiting customer...%d\n", *(int *)arg);
+            wait(&block);
         }
         else
         {
-            signal(&Rmutex);
+            eating += 1;
+            signal(&mutex);
         }
+        printf("Eating customer...%d\n", *(int *)arg);
+        sleep(3);
+        wait(&mutex);
+        eating -= 1;
+        printf("\tLeaving customer...%d\n", *(int *)arg);
+        if (eating == 0 && waiting)
+        {
+            if (waiting < 5)
+                n = waiting;
+            else
+                n = 5;
+            waiting -= n;
+            eating += n;
+            for (i = 0; i < n; i++)
+                signal(&block);
+        }
+        signal(&mutex);
+        sleep(3);
     }
 }
 
 int main()
 {
-    int i;
-    pthread_t customers[8];
-    int customer_id[8];
-
-    signal(&CSmutex);   //
-    signal(&Rmutex);    //  INITIALIZING ALL SEMAPHORES
-    signal(&EntryMutex);//
-    signal(&waiting);   //
-
-    for (i = 0; i < 8; i++)
+    int i = 0;
+    int customer_id[NO_OF_CUSTOMERS];
+    for (i = 0; i < NO_OF_CUSTOMERS; i++)
         customer_id[i] = i + 1;
 
-    for (i = 0; i < 8; i++)
+    init(&mutex, 1);
+    init(&block, 0);
+
+    for (i = 0; i < NO_OF_CUSTOMERS; i++)
         pthread_create(&customers[i], NULL, sushi_bar, &customer_id[i]);
 
-    for (i = 0; i < 8; i++)
-        pthread_join(customers[i], NULL);
+    while (1)
+        ;
 
     return 0;
 }
