c38052aca584ed3d4a6f06d43919e38c0f3fabfa
Subhajit009iitr <biswassubhajit009@gmail.com>
2023-03-15 16:08:55 +0530

    sleep time added

--- ReaderWriter.c
+++ ReaderWriter.c
@@ -1,5 +1,5 @@
 //Readers-Writers problem using Custom Semaphores and p-thread implementation (Starve-free)
-//Here we have shown the Synchronization process using only 5 Readers and 5 Writers
+//Here we have shown the Synchronization process using only 10 Readers and 10 Writers
 
 #include <pthread.h>
 #include <stdio.h>
@@ -49,7 +49,7 @@ void *reader(void* num){
     printf("Reader %d entering\n",number+1);
     wait(&Rmutex);
     Rcount++;
-    printf("Reader %d increments count to %d \n",number+1,Rcount);
+    printf("Reader %d increments Reader count to %d \n",number+1,Rcount);
     if(Rcount == 1){
         wait(&CSmutex);
         printf("Critical Section openend for Readers!!!\n");
@@ -62,11 +62,11 @@ void *reader(void* num){
      //Critical Section//
     //                //
     printf("In Critical Section Reader %d\n",number+1);
-    usleep(3);
+    sleep(3);
 
     wait(&Rmutex);
     Rcount--;
-    printf("Reader %d decrements count to %d \n",number+1,Rcount);
+    printf("Reader %d decrements Reader count to %d \n",number+1,Rcount);
     signal(&Rmutex);
     if(Rcount == 0){
         signal(&CSmutex);
@@ -86,6 +86,7 @@ void *writer(void* num){
     printf("Writer %d in Critical Section\n",number+1);
     signal(&EntryMutex);
     printf("Writer %d opens entry now\n",number+1);
+    sleep(3);
       //                //
      //Critical Section//
     //                //
@@ -97,15 +98,15 @@ void *writer(void* num){
 
 int main()
 {
-    pthread_t r[5], w[5];
-    int id[5]; // Reader-Writer process id to distinguish them
+    pthread_t r[10], w[10];
+    int id[10]; // Reader-Writer process id to distinguish them
     Rcount = 0;
 
     signal(&CSmutex);   //
     signal(&Rmutex);    //  INITIALIZING ALL SEMAPHORES
     signal(&EntryMutex);//
 
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < 10; i++) {
         id[i] = i; // assigning id
 
         pthread_create(&r[i], NULL, reader, (void *)&id[i]); // Creating pthreads for Reader processes
@@ -113,10 +114,10 @@ int main()
 
     }
     // waiting for threads to execute
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < 10; i++)
         pthread_join(r[i], NULL); 
 
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < 10; i++)
         pthread_join(w[i], NULL);
 }
 
