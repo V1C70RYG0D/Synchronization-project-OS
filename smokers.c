b2da2a54cddcfe87a8c41b0765aa27caada77f5c
Manan Garg <92792271+Coder-Manan@users.noreply.github.com>
2023-03-15 21:53:16 +0530

    Update smokers.c

--- smokers.c
+++ smokers.c
@@ -6,13 +6,6 @@
 #include <assert.h>
 #include <unistd.h>
 
-// custom semaphore struct
-typedef volatile struct
-{
-    volatile atomic_int val;
-    volatile atomic_flag lock;
-} semaphore;
-
 atomic_int onTable;
 
 char *ingredients[3] = {'Tobacco', 'Paper', 'Matches'};
@@ -25,12 +18,9 @@ void *smokerProcess(void *selfIngred)
         while (onTable != 3 - ingred)
             ;
         printf("smoking started : %d\n", ingred);
-        // printf("Smoker no. %d having %s started smoking\n", ingred, ingredients[ingred]);
         sleep(5);
         printf("smoking finished : %d\n", ingred);
         onTable = 0;
-        // sleep(5);
-        // printf("Smoker no. %d having %s finished smoking\n", ingred, ingredients[ingred]);
     }
 }
 
@@ -44,7 +34,6 @@ void *agentProcess()
         printf("Table is empty, determining ingredients to place on table\n");
         sleep(5);
         whatToPlaceOnTable = rand() % 3;
-        // printf("Placing %s and %s on table\n", ingredients[whatToPlaceOnTable], ingredients[(whatToPlaceOnTable + 1) % 3]);
         onTable = whatToPlaceOnTable + (whatToPlaceOnTable + 1) % 3;
     }
 }
